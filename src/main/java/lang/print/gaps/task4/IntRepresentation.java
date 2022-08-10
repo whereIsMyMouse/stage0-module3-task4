81ac95b5f59091cb5df0d98ac381da099a5e71bf
shariy <sharii.margarita@gmail.com>
2022-08-11 02:31:29 +0300

    commit2

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,18 +2,10 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        char charC = 'c';
-        char charH = 'h';
-        char charA = 'a';
-        char charR = 'r';
-        int intC  = charC;
-        int intH  = charH;
-        int intA  = charA;
-        int intR  = charR;
-        System.out.println(intC);
-        System.out.println(intH);
-        System.out.println(intA);
-        System.out.println(intR);
+        char charC = 99;
+        char charH = 104;
+        char charA = 97;
+        char charR = 114;
 
     }
 }
