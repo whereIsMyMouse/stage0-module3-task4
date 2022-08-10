70eccf00f6247f3da35eabc6e778a768a3752a70
shariy <sharii.margarita@gmail.com>
2022-08-11 02:19:13 +0300

    commit1

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        double a = 0;
+        double b = 0;
+        double result = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,18 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char charC = 'c';
+        char charH = 'h';
+        char charA = 'a';
+        char charR = 'r';
+        int intC  = charC;
+        int intH  = charH;
+        int intA  = charA;
+        int intR  = charR;
+        System.out.println(intC);
+        System.out.println(intH);
+        System.out.println(intA);
+        System.out.println(intR);
+
     }
 }
